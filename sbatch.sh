fcb586bac40206762dbb2a548a01f0a7898e8b87
Jeremy Kidwell <bots@jeremykidwell.info>
2019-03-25 19:19:56 +0000

    minor tweaks to htpc setup

---
+++ sbatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mail-type ALL
+#SBATCH --cpus-per-task 1
+#SBATCH --time 60:0
+#SBATCH --ntasks 20
+#SBATCH --qos bbdefault
+
+module purge; module load bluebear
+module load R/3.5.0-iomkl-2018a-X11-20180131
+module load Pandoc/2.3.1
+module load plotly/4.7.1-iomkl-2018a-R-3.5.0
+module load rgeos/0.3-28-iomkl-2018a-R-3.5.0
+module load tmap/2.2-iomkl-2018a-R-3.5.0
+
+Rscript -e 'library(rmarkdown); rmarkdown::render("mapping_draft-hpc_optimised.Rmd", "html_document")'
