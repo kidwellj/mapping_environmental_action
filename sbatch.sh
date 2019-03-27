d16a0709b2166a48ccc7c8ac352a5c785872ecb9
Jeremy Kidwell <j.kidwell@bham.ac.uk>
2019-03-27 12:08:16 +0000

    added wilderness specific script, extra poi

--- sbatch.sh
+++ sbatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mail-type ALL
 #SBATCH --cpus-per-task 1
-#SBATCH --time 120:0
+#SBATCH --time 180:0
 #SBATCH --ntasks 20
 #SBATCH --qos bbdefault
 
@@ -13,3 +13,6 @@ module load rgeos/0.3-28-iomkl-2018a-R-3.5.0
 module load tmap/2.2-iomkl-2018a-R-3.5.0
 
 Rscript -e 'library(rmarkdown); rmarkdown::render("mapping_draft-hpc_optimised.Rmd", "html_document")'
+cp mapping_draft.html /rds/projects/2016/kidwellj-01/mapping_environmental_action/
+cp figures/* /rds/projects/2016/kidwellj-01/mapping_environmental_action/figures
+cp derivedData/* /rds/projects/2016/kidwellj-01/mapping_environmental_action/derivedData
